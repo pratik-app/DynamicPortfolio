8790cf63cc6989894cf959c57e8e10f3513132a2
Pratik More <developer@pratikmore.com>
2023-01-05 17:21:27 -0500

    Icon Inserted in Title
    Icon Inserted in Title

--- Project-DynamicPortfolio/resources/views/layouts/guest.blade.php
+++ Project-DynamicPortfolio/resources/views/layouts/guest.blade.php
@@ -10,6 +10,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <!-- Icon -->
+        <link rel="icon" href="https://pratikmore.com/assets/img/favicon.ico" type="image/x-icon">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
--- Project-DynamicPortfolio/resources/views/welcome.blade.php
+++ Project-DynamicPortfolio/resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <!-- Icon -->
+        <link rel="icon" href="https://pratikmore.com/assets/img/favicon.ico" type="image/x-icon">
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
