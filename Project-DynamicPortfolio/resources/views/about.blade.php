488656a5c14f5e59f907c81fd82b16cb00c4741e
Pratik More <developer@pratikmore.com>
2023-01-05 15:53:05 -0500

    Details Updated
    Laravel Breeze is installed and a web info file is added to add it later in Readme file. This file consist of all details and tech used to create this project

---
+++ Project-DynamicPortfolio/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<head><title>About</title></head>
+<body>
+    <h1> This is the about page </h1>
+</body>
+</html>
---
+++ Project-DynamicPortfolio/resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<head><title>Contact</title></head>
+<body>
+    <h1> This is the Contact us page </h1>
+</body>
+</html>
