883dbc40110af25211c80718e28a98225094a693
Pratik More <developer@pratikmore.com>
2023-01-31 18:29:02 -0500

    Update sidebar.blade.php

--- Project-DynamicPortfolio/resources/views/admin/body/sidebar.blade.php
+++ Project-DynamicPortfolio/resources/views/admin/body/sidebar.blade.php
@@ -59,53 +59,23 @@ $adminData = App\Models\User::find($id);
                         <li><a href="{{route('contact.inbox')}}">Leads</a></li>
                     </ul>
                 </li>
-
+                <li class="menu-title">Employment</li>                
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-layout-3-line"></i>
-                        <span>Layouts</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow">Vertical</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-dark-sidebar.html">Dark Sidebar</a></li>
-                                <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
-                                <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
-                                <li><a href="layouts-boxed.html">Boxed Layout</a></li>
-                                <li><a href="layouts-preloader.html">Preloader</a></li>
-                                <li><a href="layouts-colored-sidebar.html">Colored Sidebar</a></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow">Horizontal</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-horizontal.html">Horizontal</a></li>
-                                <li><a href="layouts-hori-topbar-light.html">Topbar light</a></li>
-                                <li><a href="layouts-hori-boxed-width.html">Boxed width</a></li>
-                                <li><a href="layouts-hori-preloader.html">Preloader</a></li>
-                                <li><a href="layouts-hori-colored-header.html">Colored Header</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="menu-title">Pages</li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-account-circle-line"></i>
-                        <span>Authentication</span>
+                        <i class="ri-profile-line"></i>
+                        <span>Account Services</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="auth-login.html">Login</a></li>
-                        <li><a href="auth-register.html">Register</a></li>
-                        <li><a href="auth-recoverpw.html">Recover Password</a></li>
-                        <li><a href="auth-lock-screen.html">Lock Screen</a></li>
+                        <li><a href="#">Add New Employee</a></li>
+                        <li><a href="#">Timeline</a></li>
+                        <li><a href="#">Directory</a></li>
+                        <li><a href="#">Invoice</a></li>
+                        <li><a href="#">Error 404</a></li>
+                        <li><a href="#">Error 500</a></li>
                     </ul>
                 </li>
 
+                <li class="menu-title">Pages</li>                
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-profile-line"></i>
