db8afeb43d0a87885ea5ce83195bdafeebf07a5d
Pratik More <developer@pratikmore.com>
2023-01-31 23:29:40 -0500

    Google Chart Integration
    using google charts for data manipulation via DB

--- Project-DynamicPortfolio/resources/views/admin/account_services/employee_dashboard.blade.php
+++ Project-DynamicPortfolio/resources/views/admin/account_services/employee_dashboard.blade.php
@@ -2,34 +2,161 @@
 @section('admin')
 <!-- Jquery 3.6 -->
 <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Profit Calculator'],
+          ['Total Spent on Employees',     5000],
+          ['Total Earnings',      10000],
+          ['Profit', 5000]
+        ]);
+
+        var options = {
+            chartArea: {
+            height: '100%',
+            width: '100%',
+            
+        },
+        is3D: true,
+        legend: 'none',
+        pieHole: 0.4
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+    
 <div class="page-content">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Employee Dashboard</h4>
-                        <!-- Always use enctype multipart/form-data when dealing with Images -->
-                        <form method="post" action="#" enctype="multipart/form-data">
-                            <!-- CSRF Token is used for active user session  -->
-                            @csrf
-                            <!-- NOTE: This token is used to verify the authenticated user -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0">Employees Dashboard</h4>
+            </div>
+        </div>
+    </div>
+    <!-- end page title -->
 
-                            <input type="hidden" name="id" value="#"/>
-                        
-                        <div class="row mb-3">
-                            <label for="Title" class="col-sm-2 col-form-label">Title</label>
-                            <div class="col-sm-10">
-                                <input class="form-control" name = "title" alt="Heading Title" type="text" value="#">
+    <div class="row">
+        <div class="col-xl-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div class="flex-grow-1">
+                            <p class="text-truncate font-size-14 mb-2">Total Employees</p>
+                            <h4 class="mb-2">1452</h4>
+                            <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i class="ri-arrow-right-up-line me-1 align-middle"></i>9.23%</span>from previous period</p>
+                        </div>
+                        <div class="avatar-sm">
+                            <span class="avatar-title bg-light text-primary rounded-3">
+                                <i class="fas fa-user-tie font-size-24"></i>  
+                            </span>
+                        </div>
+                    </div>                                            
+                </div><!-- end cardbody -->
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div class="flex-grow-1">
+                            <p class="text-truncate font-size-14 mb-2">Total Cost of Employees</p>
+                            <h4 class="mb-2">938</h4>
+                            <p class="text-muted mb-0"><span class="text-danger fw-bold font-size-12 me-2"><i class="ri-arrow-right-down-line me-1 align-middle"></i>1.09%</span>from previous period</p>
+                        </div>
+                        <div class="avatar-sm">
+                            <span class="avatar-title bg-light text-success rounded-3">
+                                <i class="mdi mdi-currency-usd font-size-24"></i>  
+                            </span>
+                        </div>
+                    </div>                                              
+                </div><!-- end cardbody -->
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div class="flex-grow-1">
+                            <p class="text-truncate font-size-14 mb-2">Total Project Completed</p>
+                            <h4 class="mb-2">8246</h4>
+                            <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i class="ri-arrow-right-up-line me-1 align-middle"></i>16.2%</span>from previous period</p>
+                        </div>
+                        <div class="avatar-sm">
+                            <span class="avatar-title bg-light text-primary rounded-3">
+                                <i class="fas fa-tasks font-size-24"></i>  
+                            </span>
+                        </div>
+                    </div>                                              
+                </div><!-- end cardbody -->
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <div class="flex-grow-1">
+                            <p class="text-truncate font-size-14 mb-2">Total Earnings by Employees</p>
+                            <h4 class="mb-2">29670</h4>
+                            <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i class="ri-arrow-right-up-line me-1 align-middle"></i>11.7%</span>from previous period</p>
+                        </div>
+                        <div class="avatar-sm">
+                            <span class="avatar-title bg-light text-success rounded-3">
+                                <i class="fas fa-search-dollar font-size-24"></i>  
+                            </span>
+                        </div>
+                    </div>                                              
+                </div><!-- end cardbody -->
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title mb-4">Profit from Employees</h4>
+                    
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="text-center mt-4">
+                                <h5>3475</h5>
+                                <p class="mb-2 text-truncate">Total Hours Worked</p>
                             </div>
                         </div>
-                        </form>
+                        <!-- end col -->
+                        <div class="col-4">
+                            <div class="text-center mt-4">
+                                <h5>458</h5>
+                                <p class="mb-2 text-truncate">Total Earnings</p>
+                            </div>
+                        </div>
+                        <!-- end col -->
+                        <div class="col-4">
+                            <div class="text-center mt-4">
+                                <h5>25%</h5>
+                                <p class="mb-2 text-truncate">Total Profit</p>
+                            </div>
+                        </div>
+                        <!-- end col -->
+                    </div>
+                    <!-- end row -->
+
+                    <div class="mt-4">
+                        
+                    <div id="pieChart" class="" ></div> 
+                        
                     </div>
                 </div>
-            </div> <!-- end col -->
-        </div>
+            </div><!-- end card -->
+        </div><!-- end col -->
         
-    </div>
+    </div><!-- end row -->
 </div>
 
 @endsection
