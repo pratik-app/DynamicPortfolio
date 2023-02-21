d990488788c6a3d7c01621fc3cbabaf54f80bb03
Pratik More <developer@pratikmore.com>
2023-02-20 19:40:03 -0500

    Payroll problem Solved!
    Payroll problem and design solved

--- Project-DynamicPortfolio/resources/views/pdf.blade.php
+++ Project-DynamicPortfolio/resources/views/pdf.blade.php
@@ -5,39 +5,167 @@
     <title>Tax Information</title>
 </head>
 <body>
-    <h1>Tax Information</h1>
+    <div class="container-fluid">
 
-    <table>
-        <tr>
-            <th>Province</th>
-            <td>{{ $employeeProvince }}</td>
-        </tr>
-        <tr>
-            <th>Salary</th>
-            <td>{{ $employeeSalary }}</td>
-        </tr>
-        <tr>
-            <th>EI Deduction Rate</th>
-            <td>{{ $employeeEIDR }}</td>
-        </tr>
-        <tr>
-            <th>CPP Deduction Rate</th>
-            <td>{{ $employeeCPPR }}</td>
-        </tr>
-        <tr>
-            <th>Taxable Income</th>
-            <td>${{ $salaryAmount }}</td>
-        </tr>
-        <tr>
-            <th>Federal Tax</th>
-            <td>${{ $federalTax }}</td>
-        </tr>
-        <tr>
-            <th>Provincial Tax</th>
-            <td>{{$provincialTax}}</td>
-        </tr>
-        <tr>
-            <th>Total Tax</th>
-            <td>{{$totalTax}}</td>
-        </tr>
-        <tr>
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0">Tax Information</h4>
+                </div>
+            </div>
+        </div>
+        <!-- end page title -->
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="invoice-title">
+                                    <h4 class="float-end font-size-16"><strong>{{$employeeName}}</strong></h4>
+                                    <h3>
+                                        Company Logo Here!
+                                    </h3>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <address>
+                                            <strong>Employee Details:</strong><br>
+                                            Employee # {{$employeeID}}<br>
+                                            Employee Position: {{$employeePostion}}<br>
+                                            Employer # 3432-5<br>
+                                            Period Start # <br>
+                                            Period End # <br>
+                                            PayDay #</br>
+                                        </address>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div>
+                                    <div class="p-2">
+                                        <h3 class="font-size-16"><strong>Statement of Earnings</strong></h3>
+                                    </div>
+                                    <div class="">
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <thead>
+                                                <tr>
+                                                    <td><strong>Type</strong></td>
+                                                    <td class="text-center"><strong>Hours</strong></td>
+                                                    <td class="text-center"><strong>Rate</strong></td>
+                                                    <td class="text-center"><strong>Amount</strong></td>
+                                                    <td class="text-center"><strong>Y.T.D</strong></td>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <!-- foreach ($order->lineItems as $line) or some such thing here -->
+                                                <tr>
+                                                    <td>Salary</td>
+                                                    <td class="text-center">{{$employeeWorkHrs}}</td>
+                                                    <td class="text-center"></td>
+                                                    <td class="text-end">{{$salaryAmount}}</td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Stat $</td>
+                                                    <td class="text-center"></td>
+                                                    <td class="text-center"></td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <thead>
+                                                <tr>
+                                                    <td><strong>Type</strong></td>
+                                                    <td class="text-center"><strong>Current</strong></td>
+                                                    <td class="text-center"><strong>Y.T.D</strong></td>
+                                                    <td class="text-center"><strong>Type</strong></td>
+                                                    <td class="text-center"><strong>Current</strong></td>
+                                                    <td class="text-center"><strong>Y.T.D</strong></td>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <!-- foreach ($order->lineItems as $line) or some such thing here -->
+                                                <tr>
+                                                    <td>FED.TAX</td>
+                                                    <td class="text-center">{{$federalTax}}</td>
+                                                    <td class="text-center">{{$federalTax}}</td>
+                                                    <td class="text-end">*N/A</td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end">#</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>E.I</td>
+                                                    <td class="text-center">{{$employeeEIDeduction}}</td>
+                                                    <td class="text-center">{{$employeeEIDeduction}}</td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>C.P.P</td>
+                                                    <td class="text-center">{{$employeeCPPContribution}}</td>
+                                                    <td class="text-center">{{$employeeCPPContribution}}</td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>N/A</td>
+                                                    <td class="text-center">#</td>
+                                                    <td class="text-center">#</td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>VACACC</td>
+                                                    <td class="text-center">VacationPay</td>
+                                                    <td class="text-center">#</td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Total Days of Work </td>
+                                                    <td class="text-center">#Days</td>
+                                                    <td class="text-center">#</td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                    <td class="text-end"></td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+                                        <div class="d-print-none">
+                                            <div class="float-end">
+                                                <a href="javascript:window.print()" class="btn btn-success waves-effect waves-light"><i class="fa fa-print"></i></a>
+                                                <a href="#" class="btn btn-primary waves-effect waves-light ms-2">Send</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div> <!-- end row -->
+                        
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
+
+    </div>
+    
